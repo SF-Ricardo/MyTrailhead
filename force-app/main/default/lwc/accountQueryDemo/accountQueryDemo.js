6a3094cb23d3c76d6cc78920a15ec846e5f70c26
qinghua <qinghua.lu@aliyun.com>
2020-08-28 15:05:42 +0800

    Demo - Account list

--- force-app/main/default/lwc/accountQueryDemo/accountQueryDemo.js
+++ force-app/main/default/lwc/accountQueryDemo/accountQueryDemo.js
@@ -1,13 +1,30 @@
 import { LightningElement, wire } from 'lwc';
-import ACCOUNT_OBJECT from '@salesforce/schema/Account';
-import ID_FIELD from '@salesforce/schema/AccountId';
-import NAME_FIELD from '@salesforce/schema/Account.Name';
-import queryAccount from '@salesforce/apex/AccountDemo.QueryAccount';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account'; // 对象名
+import NAME_FIELD from '@salesforce/schema/Account.Name'; // 文本
+import Active from '@salesforce/schema/Account.Active__c'; // 下拉表
+import NumberOfEmployees from '@salesforce/schema/Account.NumberOfEmployees'; // 数字
+import OwnerName from '@salesforce/schema/Account.Owner.Name'; // lookup
+import queryAcc from '@salesforce/apex/AccountDemo.QueryAccount';
 
 export default class AccountQueryDemo extends LightningElement {
-    objectApiName = ACCOUNT_OBJECT;
-    fileds = [ID_FIELD,NAME_FIELD];
+    objectApiName = ACCOUNT_OBJECT.fieldApiName;
+    Name = NAME_FIELD.fieldApiName;
+    Active = Active.label;
+    NumberOfEmployees = NumberOfEmployees.fieldApiName;
+    OwnerId = OwnerName.Name.label;
 
-    // @wire(queryAccount)
-    // accounts
+    @wire(queryAcc)
+    accounts;
+    // accounts = [
+    //     {
+    //         id:'0X001',
+    //         Name:'zhangsan'
+    //     },{
+    //         id:'0X002',
+    //         Name:'里斯'
+    //     },{
+    //         id:'0X003',
+    //         Name:'赵高'
+    //     }
+    // ];
 }
--- force-app/main/default/lwc/hello/hello.js
+++ force-app/main/default/lwc/hello/hello.js
@@ -1,7 +1,7 @@
 import { LightningElement } from 'lwc';
 
 export default class Hello extends LightningElement {
-    world = '文本变更';
+    world = '名字的修改';
     flag = true;
     handleChange(event) {
         this.world = event.target.value;
@@ -10,4 +10,41 @@ export default class Hello extends LightningElement {
     CheckBoxChange(event){
         this.flag = event.target.checked;
     }
+
+    firstName = '';
+    lastName = '';
+
+    updateName(event) {
+        const field = event.target.name;
+        if (field === 'firstName') {
+            this.firstName = event.target.value;
+        } else if (field === 'lastName') {
+            this.lastName = event.target.value;
+        }
+    }
+
+    get uppercasedFullName() {
+        return `${this.firstName} ${this.lastName}`.trim().toUpperCase();
+    }
+
+    AccountList = [
+        {
+            Id:'1',
+            Name:'zhangsan',
+            age:14,
+            job:'工程师'
+        },
+        {
+            Id:'2',
+            Name:'李四',
+            age:19,
+            job:'画家'
+        },
+        {
+            Id:'3',
+            Name:'王五',
+            age:17,
+            job:'木匠'
+        }
+    ];
 }
