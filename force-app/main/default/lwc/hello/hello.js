3da5ef851a9b3b3238582fca3098be89663edfdd
qinghua <qinghua.lu@aliyun.com>
2020-08-28 12:55:52 +0800

    add change checkbox

--- force-app/main/default/lwc/hello/hello.js
+++ force-app/main/default/lwc/hello/hello.js
@@ -1,9 +1,13 @@
 import { LightningElement } from 'lwc';
 
 export default class Hello extends LightningElement {
-    world = '中文翻译效果呢';
-
+    world = '文本变更';
+    flag = true;
     handleChange(event) {
         this.world = event.target.value;
+    }
+
+    CheckBoxChange(event){
+        this.flag = event.target.checked;
     }
 }
